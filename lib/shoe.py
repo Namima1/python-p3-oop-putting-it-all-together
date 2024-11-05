05b32b4458e81481867cfd06191c3c3eaa943f2d
Namima Islam <namimaislam308@gmail.com>
2024-11-04 22:26:50 -0500

    complete lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size  # This will call the setter method
+        self.condition = None  # Initialize condition as None or "Used" based on your requirements
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        self.condition = "New"
